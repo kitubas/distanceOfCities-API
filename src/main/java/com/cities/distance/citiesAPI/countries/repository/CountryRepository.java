f3fa03dbdc34bb29bd6534326b415609a9fd35a3
kitubas <henrique_leite1@yahoo.com.br>
2021-08-24 10:45:48 -0300

    primeiro commit

---
+++ src/main/java/com/cities/distance/citiesAPI/countries/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.cities.distance.citiesAPI.countries.repository;
+
+import com.cities.distance.citiesAPI.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+}
